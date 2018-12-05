67bdbbd5d0078890d6c4634f0f621019100e5841
이홍석 <lhs0806@nate.com>
2018-12-05 22:43:07 +0900

    django

--- tutorial/community/views.py
+++ tutorial/community/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from tutorial.community.forms import *
+
 
 # Create your views here.
+def write(request):
+    form = Form()
+    return render(request, 'write.html', {'form': form})
+
