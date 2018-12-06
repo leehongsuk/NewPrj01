46daeff1d1faed0d371beb406a0ef02f6f22c808
이홍석 <lhs0806@nate.com>
2018-12-06 19:44:27 +0900

    경로변경

--- community/views.py
+++ community/views.py
@@ -4,7 +4,12 @@ from community.forms import *
 
 # Create your views here.
 def write(request):
-    form = Form()
+    if request.method == 'POST':
+        form = Form(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = Form()
     return render(request, 'write.html', {'form': form})
 
 
