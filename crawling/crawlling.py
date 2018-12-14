fc3eb3b1a18307fe330bdca4b829870139d05321
이홍석 <lhs0806@nate.com>
2018-12-15 00:42:55 +0900

    주석처리

--- crawling/crawlling.py
+++ crawling/crawlling.py
@@ -6,7 +6,7 @@ pip가 최신인지 확인..
 """
 from urllib.request import urlopen
 from bs4 import BeautifulSoup    # pip install BeatifulSoup4
-from selenium import webdriver
+# from selenium import webdriver
 
 html = urlopen("http://www.pythonscraping.com/pages/page1.html")
 bsObj = BeautifulSoup(html.read(), "html.parser")
